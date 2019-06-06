f33c58b331351c834566c6eba3a67574d938c2c2
Asdk <anassdaaouak72gmail.com>
2019-06-06 14:12:16 +0200

    ajout code matrice

---
+++ 00_algorithmique/01_python/exercices/jeu3.py
@@ -0,0 +1,7 @@
+import random
+import libJeu3
+
+print( libJeu3.liste_mots[0] )
+libJeu3.pendu(1) 
+reponse = libJeu3.masque("bonjour", "on")
+print(reponse)
---
+++ 00_algorithmique/01_python/exercices/liste_dico.py
@@ -0,0 +1,30 @@
+maListe =[]
+texte = "une petite phrase"
+
+print(maListe)
+maListe.append(8)
+maListe.append(5.6)
+maListe.append(2)
+print(maListe)
+
+#maListe.index(8)
+
+maListe.sort()
+
+print(maListe)
+
+texte = texte.replace("a","i")
+print(texte)
+texte : texte.replace("k","*")
+
+for element in texte:
+        texte = texte.replace(element,"*")
+
+print(texte)
+      
+phrase = "unr/autre/phrase"
+caractere = "éléphant"
+
+phrase = phrase.replace("/"," ")
+for accent in "éàè" :
+caractere = caractere.replace(element, "*")
