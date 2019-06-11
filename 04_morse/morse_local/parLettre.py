f4ca5c3ffe01c9cb3f81a64010bb9fcbaeffefee
Asdk <anassdaaouak72gmail.com>
2019-06-11 14:41:43 +0200

    modif lettre

--- 04_morse/morse_local/parLettre.py
+++ 04_morse/morse_local/parLettre.py
@@ -2,7 +2,7 @@ import ledArduino
 import dicoMatrice2D
 import comMorse
 
-print("tape un code morse")
+print("tape une lettre")
 lettre = input()
 reponseMorse = comMorse.encode(lettre)
 print("le code morse est :",reponseMorse)
