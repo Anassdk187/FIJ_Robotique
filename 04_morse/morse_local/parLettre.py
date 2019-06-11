236d560cdca4ecda7cd61b3724a0f5db846800c1
Asdk <anassdaaouak72gmail.com>
2019-06-11 14:39:15 +0200

    modif par lettre

--- 04_morse/morse_local/parLettre.py
+++ 04_morse/morse_local/parLettre.py
@@ -8,4 +8,4 @@ reponseMorse = comMorse.encode(lettre)
 print("le code morse est :",reponseMorse)
 
 reponse = comMorse.decode(reponseMorse)
-ledArduino.envoiCaractere(reponse)code
+ledArduino.envoiCaractere(reponse)
